9f1bca83eeb1984046cdd790968cd0ea4e037fed
hello2727 <jihye7369@gmail.com>
2020-01-24 00:06:27 +0900

    컨테이너 생명주기, InitializingBean/DisposableBean-@PostConstruct/@PreDestroy

---
+++ springPjtEx3/src/main/java/com/example/demo/SpringPjtEx3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPjtEx3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPjtEx3Application.class, args);
+	}
+
+}
